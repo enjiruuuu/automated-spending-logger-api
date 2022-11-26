471e9c605df2a5dfe4a63cb51dc7a5e8ccc3b82b
Enji <fightingdreamer99@gmail.com>
2022-11-27 03:01:13 +0800

    implement endpoint to update google sheet

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require('cors')
+const { google } = require("googleapis");
+
+const app = express();
+const port = 8080;
+const id='14cXyuhFZoUnIRBczdtYJQ49IDS0I4sFDiWzsiApoWU4';
+
+//This allows us to parse the incoming request body as JSON
+app.use(express.json(), cors());
+
+// With this, we'll listen for the server on port 8080
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+async function authSheets() {
+    //Function for authentication object
+    const auth = new google.auth.GoogleAuth({
+      keyFile: "keys.json",
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+  
+    //Create client instance for auth
+    const authClient = await auth.getClient();
+  
+    //Instance of the Sheets API
+    const sheets = google.sheets({ version: "v4", auth: authClient });
+  
+    return {
+      auth,
+      authClient,
+      sheets,
+    };
+  }
+
+
+  app.post("/update", async (req, res) => {
+    const { sheets } = await authSheets();
+
+    const request = {
+        spreadsheetId: id,
+        range: "Sheet1",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: req.body,
+        },
+    }
+  
+    // write rows to spreadsheet
+    try {
+        const response = (await sheets.spreadsheets.values.append(request)).data;
+        res.send({code: 200})
+    } catch (err) {
+        console.error(err);
+    }
+  });
+
+  
